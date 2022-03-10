bb15c9e4c8183ff45ab60182553355e406f922e2
kotokrad <dsl2407@gmail.com>
2022-03-10 13:45:18 +0700

    feat: interval requests

--- src/hooks/useBaseFee.ts
+++ src/hooks/useBaseFee.ts
@@ -1,8 +1,31 @@
 import { useEffect, useState } from "react";
+import { useTimeoutFn } from "react-use";
 import { fetchBaseFee, subscribeToBaseFee } from "~/services/api";
 import { BaseFee } from "~/types";
 
-function useBaseFee() {
+const REQUEST_INTERVAL = Number(process.env.REACT_APP_REQUEST_INTERVAL || 3000);
+
+function useIntervalBaseFee() {
+  const [data, setData] = useState<BaseFee>();
+  const [error, setError] = useState<Error>();
+  const [timestamp, setTimestamp] = useState(0);
+  const [_, cancel, reset] = useTimeoutFn(
+    () => setTimestamp(Date.now()),
+    REQUEST_INTERVAL
+  );
+
+  useEffect(() => {
+    fetchBaseFee()
+      .then(setData)
+      .then(() => reset())
+      .catch(setError);
+    return cancel;
+  }, [timestamp]);
+
+  return { data, error };
+}
+
+function usePollBaseFee() {
   const [data, setData] = useState<BaseFee>();
   const [error, setError] = useState<Error>();
 
@@ -14,5 +37,9 @@ function useBaseFee() {
 
   return { data, error };
 }
+
+const useBaseFee = !!Number(process.env.REACT_APP_USE_LONG_POLL || 0)
+  ? usePollBaseFee
+  : useIntervalBaseFee;
 
 export default useBaseFee;
--- src/hooks/useFeeStats.ts
+++ src/hooks/useFeeStats.ts
@@ -1,8 +1,31 @@
 import { useEffect, useState } from "react";
+import { useTimeoutFn } from "react-use";
 import { fetchFeeStats, subscribeToFeeStats } from "~/services/api";
 import { FeeStats } from "~/types";
 
-function useFeeStats() {
+const REQUEST_INTERVAL = Number(process.env.REACT_APP_REQUEST_INTERVAL || 3000);
+
+function useIntervalFeeStats() {
+  const [data, setData] = useState<FeeStats>();
+  const [error, setError] = useState<Error>();
+  const [timestamp, setTimestamp] = useState(0);
+  const [_, cancel, reset] = useTimeoutFn(
+    () => setTimestamp(Date.now()),
+    REQUEST_INTERVAL
+  );
+
+  useEffect(() => {
+    fetchFeeStats()
+      .then(setData)
+      .then(() => reset())
+      .catch(setError);
+    return cancel;
+  }, [timestamp]);
+
+  return { data, error };
+}
+
+function usePollFeeStats() {
   const [data, setData] = useState<FeeStats>();
   const [error, setError] = useState<Error>();
 
@@ -14,5 +37,9 @@ function useFeeStats() {
 
   return { data, error };
 }
+
+const useFeeStats = !!Number(process.env.REACT_APP_USE_LONG_POLL || 0)
+  ? usePollFeeStats
+  : useIntervalFeeStats;
 
 export default useFeeStats;
