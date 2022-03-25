517b5870489e54c57e7a067b3b06c2b87988ba73
kotokrad <dsl2407@gmail.com>
2022-03-25 16:25:25 +0700

    fix: chrome service worker unload

--- src/extension/chrome/background.ts
+++ src/extension/chrome/background.ts
@@ -1,5 +1,6 @@
 import { fetchBaseFee, subscribeToBaseFee } from "~/services/api";
 
+const BADGE_HIDE_TIMEOUT = 15000; // 15 seconds
 const USE_LONG_POLL = !!Number(process.env.REACT_APP_USE_LONG_POLL || 0);
 const REQUEST_INTERVAL = Number(process.env.REACT_APP_REQUEST_INTERVAL || 3000); // 3 seconds
 
@@ -13,28 +14,41 @@ async function subscribe() {
 }
 
 async function handleBaseFeeUpdate(fee: number) {
+  await chrome.storage.local.set({ baseFeeUpdated: Date.now() });
   chrome.action.setBadgeText({ text: `${fee}` });
 }
 
-function handleBaseFeeError(error: Error) {
-  console.error(error);
-  chrome.action.setBadgeText({ text: "" });
+async function handleBaseFeeError(error: Error) {
+  console.info(error);
+
+  // Hide badge after timeout
+  // unless connection is restored
+  const { baseFeeUpdated } = await chrome.storage.local.get("baseFeeUpdated");
+  const diff = Date.now() - Number(baseFeeUpdated) || Date.now();
+  if (diff > BADGE_HIDE_TIMEOUT) {
+    chrome.action.setBadgeText({ text: "" });
+  }
 }
 
 function requestBaseFee() {
   fetchBaseFee().then(handleBaseFeeUpdate).catch(handleBaseFeeError);
 }
 
+async function setAlarm() {
+  await chrome.alarms.clearAll();
+  chrome.alarms.create({ periodInMinutes: 1 });
+}
+
 chrome.action.setBadgeBackgroundColor({ color: "#21222D" });
 
 requestBaseFee();
 subscribe();
+setAlarm();
 
-// NOTE: Wake up event
+// HACK: Wake up event
 // https://stackoverflow.com/questions/66618136/persistent-service-worker-in-chrome-extension
-chrome.alarms.create({ periodInMinutes: 4.9 });
 chrome.alarms.onAlarm.addListener(() => {
-  console.log("Wake up event:", new Date().toTimeString());
+  console.log("Wake up event");
 });
 
 export {};
