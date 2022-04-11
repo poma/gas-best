4ac5eb0b15b536d313b3ade50eea9abce9a19289
kotokrad <dsl2407@gmail.com>
2022-04-11 13:16:44 +0700

    feat: include only used font variants

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "typeface-roboto";
+import "@fontsource/roboto/latin-400.css";
+import "@fontsource/roboto/latin-400-italic.css";
+import "@fontsource/roboto/latin-700.css";
 import App from "./App";
 import Providers from "./Providers";
 
